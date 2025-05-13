9f2eea9ed4e47a891735c6e4f0d80d39c4ae61dd
newton <wamitinewton@gmail.com>
2025-05-13 14:34:16 -0400

    updated spotless

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -1,11 +1,11 @@
 /**
- * Copyright (c) 2025 Meru Science Innovators Club
+ * Copyright (c) 2025 Somalink Mobile Application
  *
  * All rights reserved.
  *
- * This software is the confidential and proprietary information of Meru Science Innovators Club.
+ * This software is the confidential and proprietary information of Somalink.
  * You shall not disclose such confidential information and shall use it only in accordance
- * with the terms of the license agreement you entered into with Meru Science Innovators Club.
+ * with the terms of the license agreement you entered into with Somalink.
  *
  * Unauthorized copying of this file, via any medium, is strictly prohibited.
  * Proprietary and confidential.
